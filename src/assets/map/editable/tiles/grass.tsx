d3bf76656eb9a8caaa08d81e33c746586dabebe2
GJZ26 <adolfogj26@gmail.com>
2024-10-28 10:51:18 -0600

    feat: Adding Assets

---
+++ src/assets/map/editable/tiles/grass.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="grass" tilewidth="70" tileheight="70" tilecount="6" columns="2">
+ <image source="../../sprites/grass.png" width="140" height="210"/>
+</tileset>
---
+++ src/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "Parking_Simulator/src/core/render"
+
+func main() {
+	renderEngine := render.NewRenderEngine()
+	renderEngine.Run()
+}
